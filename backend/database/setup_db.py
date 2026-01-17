d2a7ffad6a005e113fd333dc7c64957901c8ac8f
cameron lee <cameron.lee@smus.ca>
2026-01-17 15:41:52 -0800

    add db creation scripts

---
+++ backend/database/setup_db.py
@@ -0,0 +1,51 @@
+from pymilvus import (
+    MilvusClient, 
+    FieldSchema, 
+    DataType, 
+    CollectionSchema,
+    Collection)
+import sqlite3
+
+
+client = MilvusClient("./milvus.db")
+
+fields = [
+    FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
+    FieldSchema(name="embedding",dtype=DataType.FLOAT_VECTOR, dim=512),
+    FieldSchema(name="video_id",dtype=DataType.INT64)
+]
+
+schema = CollectionSchema(fields,"Milvus Schema")
+
+client.create_collection(
+    collection_name="clip_embeddings",
+    schema=schema,
+    dimension=512
+)
+
+
+db_conn = sqlite3.connect("sqliteVideos.db")
+db_cursor = db_conn.cursor()
+db_cursor.execute("""
+                  CREATE TABLE IF NOT EXISTS videos(
+                      id INTEGER PRIMARY KEY,
+                      title TEXT,
+                      transcript TEXT,
+                      timestamp TEXT
+                  )
+                  """)
+
+db_conn.commit()
+db_conn.close()
+
+db_conn = sqlite3.connect("sqliteTags.db")
+db_cursor = db_conn.cursor()
+db_cursor.execute("""
+                  CREATE TABLE IF NOT EXISTS tags(
+                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      tag TEXT,
+                      video_id INTEGER
+                  )
+                  """)
+db_conn.commit()
+db_conn.close()

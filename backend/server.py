f98dde0a9d989428f02ec1dc59a9aa33a3d619d0
jzhong16 <jooleyaz@gmail.com>
2026-01-17 17:50:01 -0800

    endpoints

--- backend/server.py
+++ backend/server.py
@@ -6,13 +6,23 @@ from objects.ResponseObjects import ResponseTagsObject, ResponseVideoObject
 app = FastAPI()
 
 # upload video
-@app.post("/api/video")
+@app.post("/api/videos")
 async def create_video(video: RequestVideoObject):
-    print("CREATED VIDEO")
+    # SQL INSERTION/CREATION QUERY HERE
     return video
 
-# get video metadata
-@app.get("/videos")
-def get_videos():
-    print("GOT VIDEOS")
-    return # DB 
+# get all videos
+@app.get("/api/videos")
+def get_videos(limit: int = 50, offset: int = 0):
+    # SQL DATABASE QUERY HERE
+    return
+
+# retrieve full metadata + transcript for a specific video
+@app.get("/api/video/:videoId")
+def get_video(videoId):
+    # SQL DATANASE QUERY HERE
+    return
+
+@app.get("/api/search")
+def search(search: RequestSearchObject):
+    return
